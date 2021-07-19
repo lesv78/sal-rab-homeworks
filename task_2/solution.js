8afafb5e1d5462bdec468c8342e48b985ccb33d3
Svetlana Nikulina <nikulina_s_v@mail.ru>
2021-07-19 08:53:50 +0500

    task_2 don2

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,13 +9,13 @@ function calcShipping(sum, min, shipping) {
      let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-     if productsSum=0 {shippingSum=0};
+     if (productsSum==0) {shippingSum=0};
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-     if productsSum>=freeShippingMinSum {shippingSum=0};
+     if (productsSum>=freeShippingMinSum) {shippingSum=0};
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-     if productsSum>0 and productsSum<freeShippingMinSum {shippingSum=shippingSum};
+     if (productsSum>0 && productsSum<freeShippingMinSum) {shippingSum=shippingPrice};
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -33,7 +33,7 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-     if productsSum>=discountMinSum {discountSum=productsSum*discountPart/100} else discountSum=0;
+     if (productsSum>=discountMinSum) {discountSum=productsSum*discountPart/100} else discountSum=0;
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -59,7 +59,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
      let freeShipping;
-     shippingSum=0;//true;
+     shippingSum==0;//true;
     shippingSum!=0;//false;
     // Конец решения задачи №2.3.
 
