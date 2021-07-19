01b8c83b665fe7e07428aa23c582de34f4b2553c
Svetlana Nikulina <nikulina_s_v@mail.ru>
2021-07-19 09:32:01 +0500

    task_2 don3

--- task_2/solution.js
+++ task_2/solution.js
@@ -33,7 +33,7 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-     if (productsSum>=discountMinSum) {discountSum=productsSum*discountPart/100} else discountSum=0;
+     if (productsSum>=discountMinSum) {discountPrice=productsSum*discountPart/100} else discountPrice=0;
     // Конец решения задания №2.2.
 
     return discountPrice;
