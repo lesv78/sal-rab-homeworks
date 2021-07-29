70e077fbbd9efc1a7cba9b95563dd0f281ffaa1b
Svetlana Nikulina <nikulina_s_v@mail.ru>
2021-07-30 02:35:23 +0500

    task_final don30

--- task_final/solution.js
+++ task_final/solution.js
@@ -64,17 +64,23 @@ function sendRequest(name, phone, street, house, entrance, floor, flat, goods, s
 }*/
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {}, goods: [], order: {}};
+
     let countOfGoods = goods.length;
+
     for (let i = 0; i < countOfGoods; i++) {
+        goods[i]={title:goods[i].title, count: goods[i].count}
         data.goods.push(goods[i].title);
     }
-    goods[i]={title:goods[i].title, count: goods[i].count};
 
-    data.order.address = ул. ${address.street}, дом ${address.house}, 
-    ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat};
+    
+
+    data.order.address = 'ул. ' ${address.street}, 'дом ' ${address.house}, 
+    ${address.entrance} ' подъезд', ${address.floor} ' этаж', 'кв ' ${address.flat};
     ;
     data.order.sum = sum;
     data.client = name + ' ' + phone;
+
     let jsonData = JSON.stringify(data);
+
     return jsonData;
 }
