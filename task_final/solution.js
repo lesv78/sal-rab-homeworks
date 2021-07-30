683195557c9bad73ba2c534d9f51cce2bfa9ce8d
Svetlana Nikulina <nikulina_s_v@mail.ru>
2021-07-30 13:42:42 +0500

    task_final don50

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,18 +40,11 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i++) {
-      /*  goods[i]={title:goods[i].title, count: goods[i].count};
-        data.goods.push(goods[i].title)*/
         data.goods.push({title:goods[i].title, count: goods[i].count}) 
     };
 
-   /* data.order.address = `ул.`+{street} + `, дом ` + {house} + 
-    {entrance} + ` подъезд,` + {floor} + ` этаж` + `, кв ` + {flat};*/
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     
-
-    data.order.address = `ул. ${address.street}, дом  ${address.house}, 
-    ${address.entrance} подъезд, ${address.floor}  этаж, кв  ${address.flat}`;
-    ;
 
     data.order.sum = sum;
     data.client = name + ' ' + phone;
